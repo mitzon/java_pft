be72a6ce36982c6fe01dd994c7271f0de7e597ed
mitzon <mitzon@yandex.ru>
2022-04-23 18:26:57 +0300

    add PointTests.java

---
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/PointTests.java
@@ -0,0 +1,25 @@
+package ru.stqa.pft.sandbox;
+
+import jdk.jfr.Description;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+
+public class PointTests {
+
+    @Test
+    @Description("Проверяем, что метод distanceToAnotherPoint отрабатывает корректно;" +
+            "так же проверяем, расстояние между точками рассчитывается всегда одинаковое.")
+    public void distanceTest(){
+        Point p1 = new Point("p1", 6, 9);
+        Point p2 = new Point("p2", 2, 5);
+        double correctDistance = 5.656854249492381;
+
+        SoftAssert softAssert = new SoftAssert();
+        softAssert.assertEquals(p1.distanceToAnotherPoint(p2), correctDistance, "First case failed because of: ");
+        softAssert.assertEquals(p2.distanceToAnotherPoint(p1), correctDistance, "Second case failed because of: ");
+        softAssert.assertAll();
+
+
+
+    }
+}
