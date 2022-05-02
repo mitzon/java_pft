cd8e554fece0b137e7caab6bd71e48bbe007d953
mitzon <mitzon@yandex.ru>
2022-05-02 21:02:46 +0300

    optimize imports

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/GroupCreationTests.java
@@ -1,10 +1,6 @@
 package ru.stqa.pft.addressbook;
 
-import java.util.concurrent.TimeUnit;
-
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.testng.annotations.*;
-import org.openqa.selenium.*;
+import org.testng.annotations.Test;
 
 
 public class GroupCreationTests extends BaseTest {
