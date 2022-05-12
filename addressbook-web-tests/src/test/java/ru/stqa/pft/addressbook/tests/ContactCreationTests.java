9914d184c11c8a0af5bb9ce5dbb88e6b90219b8f
mitzon <mitzon@yandex.ru>
2022-05-12 17:07:26 +0300

    delete comment

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -9,14 +9,6 @@ public class ContactCreationTests extends BaseTest {
     @Test
     public void testContactCreation() throws Exception {
 
-//        ContactData contactData = new ContactData(
-//                "John",
-//                "Doe",
-//                "+79619854569",
-//                "smth@yandex.ru",
-//                "Matrosova street, 16/2",
-//                "test1");
-
         app.getContactHelper().gotoAddNew();
         app.getContactHelper().createContact(ContactData.contactData);
         app.getSessionHelper().logout();
