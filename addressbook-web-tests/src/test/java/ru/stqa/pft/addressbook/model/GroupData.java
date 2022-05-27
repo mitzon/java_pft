a507f9e6e631f8e3670a252efb257027694e06cf
mitzon <mitzon@yandex.ru>
2022-05-27 16:50:06 +0300

    lesson 4.10

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
@@ -18,13 +18,6 @@ public class GroupData {
         this.footer = footer;
     }
 
-    public GroupData(String name, String header, String footer) {
-        this.id = 0;
-        this.name = name;
-        this.header = header;
-        this.footer = footer;
-    }
-
     public int getId() {
         return id;
     }
@@ -42,24 +35,6 @@ public class GroupData {
     }
 
     @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        GroupData groupData = (GroupData) o;
-
-        if (id != groupData.id) return false;
-        return name != null ? name.equals(groupData.name) : groupData.name == null;
-    }
-
-    @Override
-    public int hashCode() {
-        int result = id;
-        result = 31 * result + (name != null ? name.hashCode() : 0);
-        return result;
-    }
-
-    @Override
     public String toString() {
         return "GroupData{" +
                 "id='" + id + '\'' +
@@ -67,4 +42,25 @@ public class GroupData {
                 '}';
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        GroupData groupData = (GroupData) o;
+
+        return name != null ? name.equals(groupData.name) : groupData.name == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return name != null ? name.hashCode() : 0;
+    }
+
+    public GroupData(String name, String header, String footer) {
+        this.id = Integer.MAX_VALUE;
+        this.name = name;
+        this.header = header;
+        this.footer = footer;
+    }
 }
