c7c57dfe02c70d02f76d1099dd20a8e0036370df
mitzon <mitzon@yandex.ru>
2022-05-27 15:58:10 +0300

    lesson 4.6

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupDeletionTests.java
@@ -8,23 +8,25 @@ import java.util.List;
 
 public class GroupDeletionTests extends BaseTest {
 
-  @Test
-  public void testGroupDeletion() throws Exception {
-    app.getNavigationHelper().gotoGroupPage();
+    @Test
+    public void testGroupDeletion() throws Exception {
+        app.getNavigationHelper().gotoGroupPage();
 
-    if (! app.getGroupHelper().isThereAGroup()) {
-      app.getGroupHelper().createGroup(new GroupData("new_group", "new2", "new3"));
+        if (!app.getGroupHelper().isThereAGroup()) {
+            app.getGroupHelper().createGroup(new GroupData("new_group", "new2", "new3"));
+        }
+
+        List<GroupData> before = app.getGroupHelper().getGroupList();
+        app.getGroupHelper().selectGroup(0);
+        app.getGroupHelper().deleteSelectedGroups();
+        app.getGroupHelper().returnToGroupPage();
+
+        List<GroupData> after = app.getGroupHelper().getGroupList();
+        Assert.assertEquals(after.size(), before.size() - 1);
+
+        before.remove(0);
+        Assert.assertEquals(before, after);
+
     }
-
-    List<GroupData> before = app.getGroupHelper().getGroupList();
-//    int before = app.getGroupHelper().getGroupCount();
-    app.getGroupHelper().selectGroup(0);
-    app.getGroupHelper().deleteSelectedGroups();
-    app.getGroupHelper().returnToGroupPage();
-
-    List<GroupData> after = app.getGroupHelper().getGroupList();
-//    int after = app.getGroupHelper().getGroupCount();
-    Assert.assertEquals(after.size(), before.size() - 1);
-  }
 
 }
