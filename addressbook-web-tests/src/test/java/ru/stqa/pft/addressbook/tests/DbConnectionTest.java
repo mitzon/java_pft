e38767498b6ee094607c3c01c16e856e78e9bc5c
mitzon <mitzon@yandex.ru>
2022-06-26 13:01:21 +0300

    lesson 7.1

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/DbConnectionTest.java
@@ -0,0 +1,35 @@
+package ru.stqa.pft.addressbook.tests;
+
+import org.testng.annotations.Test;
+import ru.stqa.pft.addressbook.model.GroupData;
+import ru.stqa.pft.addressbook.model.Groups;
+
+import java.sql.*;
+
+public class DbConnectionTest {
+    @Test
+    public void testDbConnection() {
+        Connection conn = null;
+        try {
+            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/addressbook?" +
+                    "user=root&password=");
+            Statement st = conn.createStatement();
+            ResultSet rs = st.executeQuery("select group_id,group_name,group_header,group_footer from group_list");
+            Groups groups = new Groups();
+            while (rs.next()) {
+                groups.add(new GroupData()
+                        .withId(rs.getInt("group_id"))
+                        .withFooter(rs.getString("group_footer"))
+                        .withHeader(rs.getString("group_header"))
+                        .withName(rs.getString("group_name")));
+            }
+            rs.close();
+            st.close();
+            conn.close();
+            System.out.println(groups);
+
+        } catch (SQLException ex) {
+            System.out.println("Ex: " + ex.getMessage());
+        }
+    }
+}
