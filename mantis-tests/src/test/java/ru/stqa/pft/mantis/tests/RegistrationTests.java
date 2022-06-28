1f0dccf16084e45f5956426bc6b9232e80ce2044
mitzon <mitzon@yandex.ru>
2022-06-28 13:43:14 +0300

    lesson 8.4

---
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/RegistrationTests.java
@@ -0,0 +1,12 @@
+package ru.stqa.pft.mantis.tests;
+
+import org.testng.annotations.Test;
+
+public class RegistrationTests extends BaseTest {
+
+    @Test
+    public void testRegistration() {
+        app.registration().start("", "@");
+
+    }
+}
